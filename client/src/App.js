2d346641e73e4e83a96daa2d6d93e3d7784aa944
ScxttM <alan.montesinos@uabc.edu.mx>
2022-11-07 13:13:03 -0800

    done

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
-import Pusher from 'pusher-js';
-import format from 'date-fns/format';
-import './App.css';
+import React, { Component } from "react";
+import Pusher from "pusher-js";
+import format from "date-fns/format";
+import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
     const urlParams = new URLSearchParams(window.location.search);
-    const isUserAuthorized = urlParams.has('authorized') ? true : false;
+    const isUserAuthorized = urlParams.has("authorized") ? true : false;
 
     this.state = {
       isUserAuthorized,
@@ -19,27 +19,27 @@ class App extends Component {
     const { isUserAuthorized } = this.state;
 
     if (isUserAuthorized) {
-      fetch('http://localhost:5000/history')
-        .then(res => res.json())
-        .then(data => {
+      fetch("http://localhost:5000/history")
+        .then((res) => res.json())
+        .then((data) => {
           this.setState({
             musicHistory: data,
           });
         })
-        .catch(error => console.log(error));
+        .catch((error) => console.log(error));
 
-      const pusher = new Pusher('6dd16304a8d9e90f8877', {
-        cluster: 'eu',
+      const pusher = new Pusher("9c17e799559df50bf087", {
+        cluster: "us3",
         encrypted: true,
       });
 
-      const channel = pusher.subscribe('spotify');
-      channel.bind('update-history', data => {
-        this.setState(prevState => {
+      const channel = pusher.subscribe("spotify");
+      channel.bind("update-history", (data) => {
+        this.setState((prevState) => {
           const arr = data.musicHistory
-            .map(item => {
+            .map((item) => {
               const isPresent = prevState.musicHistory.find(
-                e => e.played_at === item.played_at
+                (e) => e.played_at === item.played_at
               );
               if (isPresent === undefined) {
                 return item;
@@ -59,7 +59,7 @@ class App extends Component {
   render() {
     const { isUserAuthorized, musicHistory } = this.state;
     const connectSpotify = isUserAuthorized ? (
-      ''
+      ""
     ) : (
       <a href="http://localhost:5000/login">Connect your Spotify account</a>
     );
@@ -68,7 +68,7 @@ class App extends Component {
       <tr key={item.played_at}>
         <td>{index + 1}</td>
         <td>{item.track_name}</td>
-        <td>{format(item.played_at, 'D MMM YYYY, hh:mma')}</td>
+        <td>{format(item.played_at, "D MMM YYYY, hh:mma")}</td>
       </tr>
     );
 
